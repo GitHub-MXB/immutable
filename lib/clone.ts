4b761dad7ec49beb561931ba8802ec6b0a9dc4b9
unknown <2488275790@qq.com>
2020-06-05 18:27:35 +0800

    完善

--- lib/clone.ts
+++ lib/clone.ts
@@ -1,6 +1,9 @@
 /**
+ *
+ * Shallow copy an object. The current version of the object container only supports array, set, map and object types
+ *
  * 浅复制一个对象，当前版本对象容器仅支持 Array,Set,Map,Object 类型
- * @param target 目标对象
+ * @param target Target object 目标对象
  */
 let assign = (target: any[] | object): object | any[] => {
     if (target instanceof Array) return target.concat()
@@ -12,28 +15,30 @@ let assign = (target: any[] | object): object | any[] => {
 type mapType<T> = Map<any, { parent: T; key: string | number | symbol }>
 
 /**
+ * Deep clone an object
  * 深度克隆一个对象
- * @param target 目标对象
- * @param callback 回调操作
+ * @param target
+ * @param callback
  */
 function deepClone<T extends object>(target: T, callback: (x: T) => any) {
-    let map: mapType<T> = new Map() //储存
-    let real = assign(target) //浅克隆目标对象
-    callback.call(real, Clone.create(<T>real, map)) //执行回调，实际监听real对象
+    let map: mapType<T> = new Map() //Store target and its real address 储存 target 和其真实地址
+    let real = assign(target) //Shallow clone target object is real object 浅克隆目标对象为 real 对象
+    callback.call(real, Clone.create(<T>real, map)) // Actually listening to real objects. 执行回调，实际监听real对象
     return real
 }
 
 class Clone<T extends object> {
-    proxy: T //用于储存Proxy对象
+    proxy: T //Used to store proxy objects  用于储存Proxy对象
     constructor(obj: T, map: mapType<T>) {
         let clone = new Proxy(obj, {
             get(target, prop) {
                 let val = target[prop]
-                map.set(val, { parent: target, key: prop }) //将读取的值储存，set时，会修改该值
+                map.set(val, { parent: target, key: prop }) //Store Val and its real address, which will be used in set 将 val 与其真实地址储存，set 时会用到
                 switch (typeof val) {
                     case "object":
-                        target[prop] = assign(val) //将读取的值浅克隆并替换
-                        return Clone.create(target[prop], map) //返回代理的值
+                        target[prop] = assign(val)
+                        //The read value is shallowly cloned and assigned, and the new proxy object is returned  将读取的值浅克隆并赋值，返回新的代理对象
+                        return Clone.create(target[prop], map)
                     case "function":
                         return val.bind(target)
                     default:
@@ -41,7 +46,7 @@ class Clone<T extends object> {
                 }
             },
             set(target, prop, value) {
-                let obj = map.get(target) //获取这个值有没有get过，若有，将要设置的对象浅克隆并替换，然后赋值
+                let obj = map.get(target) //When set is triggered, get the real address of target (the value of target cannot be assigned directly)  触发 set 时，获取 target 的真实地址（target 的值不能直接赋值），
                 if (obj) {
                     let { parent, key } = obj
                     parent[key] = assign(target)
